664ec49b17860730707c50a53def833696b940bc
fadhlimulyana20 <baba.fadhli@gmail.com>
2020-09-12 23:26:36 +0700

    Add blog list page and blog detail page

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
 # Create your views here.
+class PostList(ListView):
+    template_name = "home.html"
+    queryset = Post.objects.filter(status=1).order_by('-created_at')
+
+class PostDetail(DetailView):
+    template_name = "post-detail.html"
+    model = Post
