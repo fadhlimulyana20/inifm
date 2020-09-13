676351ab396e0752a540852e1ecae403925edd10
fadhlimulyana20 <baba.fadhli@gmail.com>
2020-09-13 15:02:36 +0700

    Add featured image

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,10 @@ STATUS = (
     (1, 'Published')
 )
 
+def post_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    return 'posts/{0}/{1}'.format(instance.id, filename)
+
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -15,6 +19,7 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     content = models.TextField()
     status = models.IntegerField(choices=STATUS, default=0)
+    featured_image = models.ImageField(upload_to=post_directory_path, null=True, blank=True)
 
     class Meta:
         ordering = ['-created_at']
