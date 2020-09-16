d25deaf071cc6bed7b82b2ada9b6a7861619ac6e
fadhlimulyana20 <baba.fadhli@gmail.com>
2020-09-16 17:29:06 +0700

    Add navbar active and footer

--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,21 @@ class PostList(ListView):
     template_name = "home.html"
     queryset = Post.objects.filter(status=1).order_by('-created_at')
 
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in the publisher
+        context['posts'] = self.queryset
+        context['route'] = 'blog'
+        return context
+
 class PostDetail(DetailView):
     template_name = "post-detail.html"
     model = Post
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in the publisher
+        context['route'] = 'blog'
+        return context
