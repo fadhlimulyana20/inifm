1c99afc403e11d29e55492a94142e1f22c8c4f5b
fadhlimulyana20 <baba.fadhli@gmail.com>
2020-09-17 11:54:48 +0700

    Add Pagination

--- blog/views.py
+++ blog/views.py
@@ -5,13 +5,13 @@ from .models import Post
 # Create your views here.
 class PostList(ListView):
     template_name = "home.html"
+    paginate_by = 5
     queryset = Post.objects.filter(status=1).order_by('-created_at')
 
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get a context
         context = super().get_context_data(**kwargs)
         # Add in the publisher
-        context['posts'] = self.queryset
         context['route'] = 'blog'
         return context
 
